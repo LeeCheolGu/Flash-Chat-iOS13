beb4b36702f97d645eebabd7346a3ca9b1cccb4f
LeeCheolGu <cjfrn0443@gmail.com>
2020-10-19 19:53:01 +0900

    Update ChatViewController & Models

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by 이철구 on 2020/10/19.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
